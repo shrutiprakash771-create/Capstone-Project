bbb0c8a2e1c123c6758f566b71d47e96f8378182
Shruti Prakash <shrutiprakash771@gmail.com>
2026-05-10 13:31:59 +0530

    Add files via upload

--- planner/logic.py
+++ planner/logic.py
@@ -3,7 +3,7 @@ import random
 
 logic = Flask(__name__)
 
-#   LOGIC
+# LOGIC
 
 def generate_timetable(data):
 
@@ -28,13 +28,16 @@ def generate_timetable(data):
 
     if not subjects:
         subjects = ["general study"]
+
     if not weak:
         weak = subjects.copy()
+
     if not strong:
         strong = subjects.copy()
 
     strong = [s for s in strong if s not in weak]
     normal = [s for s in subjects if s not in weak and s not in strong]
+
     if not normal:
         normal = subjects.copy()
 
@@ -54,12 +57,14 @@ def generate_timetable(data):
     output = []
 
     def pick_subject():
-        r = random.random() # creates a random decimal number between 0 and 1
-        if r < 0.5: # 50% chance to pick from weak subjects
+        r = random.random()
+
+        if r < 0.5:
             return random.choice(weak)
-        elif r < 0.8: #30% normal b/w 0.5 and 0.8
+        elif r < 0.8:
             return random.choice(normal)
-        return random.choice(strong) # 20% strong
+
+        return random.choice(strong)
 
     for d in range(1, days + 1):
         output.append(f"DAY {d}")
@@ -74,18 +79,28 @@ def generate_timetable(data):
 
         while current < end_day:
 
-            if (total_study_done >= study_minutes_goal and total_break_done >= break_minutes_goal):
+            if (
+                total_study_done >= study_minutes_goal
+                and total_break_done >= break_minutes_goal
+            ):
                 break
 
-            # Meals
+            # MEALS 
             for name, (s, e) in meals.items():
-                if s and not meal_done[name] and s <= current < e:
-                    output.append(f"{name}: {format_time(current)} - {format_time(e)}")
+                if (
+                    s is not None
+                    and not meal_done[name]
+                    and s <= current < e
+                ):
+                    output.append(
+                        f"{name}: {format_time(current)} - {format_time(e)}"
+                    )
                     current = e
                     meal_done[name] = True
 
-            # Study
+            # STUDY
             if total_study_done < study_minutes_goal:
+
                 subject = pick_subject()
                 duration = base_duration
 
@@ -94,17 +109,60 @@ def generate_timetable(data):
                 elif subject in strong:
                     duration -= 10
 
-                duration = max(25, min(duration, study_minutes_goal - total_study_done)) # min 25 min = default 
+                duration = max(
+                    25,
+                    min(duration, study_minutes_goal - total_study_done)
+                )
 
-                output.append(f"{subject.title()}: {format_time(current)} - {format_time(current+duration)}")
+                # if study would cross a meal
+                meal_interrupted = False
+
+                for name, (s, e) in meals.items():
+
+                    if (
+                        s is not None
+                        and not meal_done[name]
+                        and current < s < current + duration
+                    ):
+                        output.append(
+                            f"{subject.title()}: {format_time(current)} - {format_time(s)}"
+                        )
+
+                        total_study_done += (s - current)
+                        current = s
+
+                        output.append(
+                            f"{name}: {format_time(current)} - {format_time(e)}"
+                        )
+
+                        current = e
+                        meal_done[name] = True
+
+                        session_count += 1
+                        meal_interrupted = True
+                        break
+
+                if meal_interrupted:
+                    continue
+
+                output.append(
+                    f"{subject.title()}: {format_time(current)} - {format_time(current + duration)}"
+                )
 
                 current += duration
                 total_study_done += duration
                 session_count += 1
 
                 if session_count >= break_after:
-                    b = min(base_break, break_minutes_goal - total_break_done)
-                    output.append(f"Break: {format_time(current)} - {format_time(current+b)}")
+                    b = min(
+                        base_break,
+                        break_minutes_goal - total_break_done
+                    )
+
+                    output.append(
+                        f"Break: {format_time(current)} - {format_time(current + b)}"
+                    )
+
                     current += b
                     total_break_done += b
                     session_count = 0
@@ -113,11 +171,13 @@ def generate_timetable(data):
 
     return output
 
-# ------------------ ROUTES ------------------
+
+#  ROUTES 
 
 @logic.route("/")
 def index():
     return render_template("index.html")
+
 
 @logic.route("/result", methods=["POST"])
 def result():
@@ -125,11 +185,13 @@ def result():
     timetable = generate_timetable(data)
     return render_template("result.html", timetable=timetable)
 
+
 @logic.route("/stats")
 def stats():
     return render_template("stats.html")
 
-# ----- RUN --------
+
+# RUN 
 
 if __name__ == "__main__":
     logic.run(debug=True)
